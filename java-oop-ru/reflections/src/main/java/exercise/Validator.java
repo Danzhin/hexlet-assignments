ed5511a956800e7d665fe22a5cc0d04bb4636102
@hexlet/cli <support@hexlet.io>
2024-10-29 17:03:58 +0300

    submit java-oop-ru/reflections

---
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -0,0 +1,29 @@
+package exercise;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+// BEGIN
+class Validator {
+    public static List<String> validate(Object obj) {
+        List<String> notValidFields = new ArrayList<>();
+        
+        Field[] fields = obj.getClass().getDeclaredFields();
+        
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(NotNull.class)) {
+                field.setAccessible(true);
+                try {
+                    if (field.get(obj) == null) {
+                        notValidFields.add(field.getName());
+                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        return notValidFields;
+    }
+}
+// END
